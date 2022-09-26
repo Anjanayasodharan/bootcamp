4297e3f6a4690010b2c1225429a728026134e3e4
Anjanayasodharan <anjanayasodharan89@gmail.com>
2022-09-26 17:08:31 +0530

    employees commit

---
+++ src/com/Nest/employee/employee.java
@@ -0,0 +1,35 @@
+package com.Nest.employee;
+
+import java.util.Scanner;
+
+public class employee {
+    public static void main(String[] args){
+        int choice;
+        while(true)
+        {
+            System.out.println("Select an option");
+            System.out.println("1.Add employee");
+            System.out.println("2.View employee");
+            System.out.println("5.Exit");
+            Scanner opt=new Scanner(System.in);
+            choice=opt.nextInt();
+
+
+            switch (choice)
+            {
+                case 1:
+                    System.out.println("add employee");
+                    break;
+                case 2:
+                    System.out.println("view employee");
+                    break;
+
+                case 3:
+                    System.exit(0);
+                    break;
+            }
+        }
+    }
+}
+
+
